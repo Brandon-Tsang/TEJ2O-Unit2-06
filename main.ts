c83bc49ee3fc1c02d80555e0cd4b4cfe65d11f97
Brandon-Tsang <brandon.tsang@stu.ocsb.ca>
2026-02-26 13:22:52 -0500

    Turn on and off the LED

--- main.ts
+++ main.ts
@@ -5,15 +5,18 @@
  * This program can let you turn on and off the LED.
 */
 
+// Clear Screen and Happy Face.
 basic.clearScreen()
 pins.digitalWritePin(DigitalPin.P16, 0)
 basic.showIcon(IconNames.Happy)
 
+// Turn on the LED
 input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Yes)
     pins.digitalWritePin(DigitalPin.P16, 1)
 })
 
+// Turn off the LED
 input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.No)
     pins.digitalWritePin(DigitalPin.P16, 0)
